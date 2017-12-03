c695b2dfb9fcb97a31a63f2ea54f40d0af37a9ef
Charlotte Wilson <charlottead@live.co.uk>
2017-12-03 23:08:28 +0000

    comments

--- december_03/problem_2.py
+++ december_03/problem_2.py
@@ -22,22 +22,22 @@ def _move_down(x, y):
 
 
 def _move_right_diagonal_up(x, y):
-    """ Move right on the grid """
+    """ Move right diagonal up on the grid """
     return x + 1, y + 1
 
 
 def _move_left_diagonal_up(x, y):
-    """ Move left on the grid """
+    """ Move left diagonal up on the grid """
     return x - 1, y + 1
 
 
 def _move_right_diagonal_down(x, y):
-    """ Move up on the grid """
+    """ Move right diagonal down the grid """
     return x + 1, y - 1
 
 
 def _move_left_diagonal_down(x, y):
-    """ Move down on the grid """
+    """ Move left diagonal down on the grid """
     return x - 1, y - 1
 
 
